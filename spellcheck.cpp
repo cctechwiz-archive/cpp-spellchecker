efc06213d31978f517b5afc3e3812dd2da53cea2
Josh Maxwell <cctechwiz@gmail.com>
2014-11-20 19:45:17 -0700

    Created loadDictionary() function to load the dictionary initially.

--- spellcheck.cpp
+++ spellcheck.cpp
@@ -7,11 +7,44 @@
 
 using namespace std;
 
+MySpellCheckDictionary loadDictionary(char *filename);
+
 int main(int argc, char* argv[]){
+	char filename[] = "words.txt";
+	MySpellCheckDictionary hashDictionary = loadDictionary(filename);
+
+	hashDictionary.printDictionary();
+	
+	// if(argc > 1){
+	// 	FILE * fileToSpellCheck;
+	// 	fileToSpellCheck = fopen(argv[1], "r");
+		
+	// 	FILE * checkedFile;
+	// 	char fileOutputName[] = "checked_%s", argv[1];
+	// 	checkedFile = fopen(fileOutputName, "w");
+		
+	// 	while(!feof(fileToSpellCheck)){
+	// 		char * word;
+	// 		fscanf(fileToSpellCheck,"%s", word);
+	// 		string wordstr(word);
+			
+	// 		if(hashDictionary.spellCheck(wordstr)){
+	// 			fprintf(checkedFile, "%s", word);
+	// 		}
+	// 		else {
+	// 			fprintf(checkedFile, "*^*%s*^*", word);
+	// 		}
+	// 	}
+	// }
+	
+}
+
+
+MySpellCheckDictionary loadDictionary(char *filename){
 	MySpellCheckDictionary hashDictionary;
 
 	FILE * in;
-	in = fopen("words.txt", "r");
+	in = fopen(filename, "r");
 
 	while(!feof(in)){
 		char *temp = new char[65000];
@@ -20,30 +53,5 @@ int main(int argc, char* argv[]){
 		hashDictionary.addToDictionary(str);
 	}
 
-	hashDictionary.printDictionary();
-	
-	// if(argc > 1){
-		// FILE * fileToSpellCheck;
-		// fileToSpellCheck = fopen(argv[1], "r");
-		
-		// FILE * checkedFile;
-		// char fileOutputName[] = "checked_%s", argv[1];
-		// checkedFile = fopen(fileOutputName, "w");
-		
-		// while(!feof(fileToSpellCheck)){
-			// char * word;
-			// fscanf(fileToSpellCheck,"%s", word);
-			// string wordstr(word);
-			
-			// if(hashDictionary.spellCheck(wordstr)){
-				// fprintf(checkedFile, "%s", word);
-			// }
-			// else {
-				// fprintf(checkedFile, "*^*%s*^*", word);
-			// }
-		// }
-	// }
-	
-	
-	
+	return hashDictionary;
 }
